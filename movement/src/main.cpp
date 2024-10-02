e23bf313082a0d5ff9824686ac0ae3eb3ee360bc
abdelrahman <abdelrahman.y.salah@gmail.com>
2024-10-02 18:20:31 +0300

    solved the micro ros problem

--- movement/src/main.cpp
+++ movement/src/main.cpp
@@ -1,17 +1,25 @@
 #include <Arduino.h>
-#include <micro_ros_arduino.h>
+#include <micro_ros_platformio.h>
+
+
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <rcl/rcl.h>
+
 #include <std_msgs/msg/float32.h>
 #include <std_msgs/msg/string.h>
-#include <Wifi.h>
+#include <string>
+
+
+using namespace std;
 
 const char *ssid = "wifi name";   // name to be changed
 const char *password =  "password";  // password to be changed
 
-#define leftMotor_neg 33
-#define leftMotor_pos 15
-#define rightMotor_neg 14
-#define rightMotor_pos 5
-#define imu_sensor 8       
+#define leftMotor_neg 9
+#define leftMotor_pos 8
+#define rightMotor_neg 13
+#define rightMotor_pos 12
 
 rcl_publisher_t publisher;   
 
@@ -32,13 +40,25 @@ int left_speed=0;
 int right_speed=0;
 String direction = "stop";
 
+
+  void moveforward(int left,int right);
+  void movebackward(int left,int right);
+  void turnleft(int left,int right);
+  void turnright(int left,int right);
+  void stop();
+  void motor_left_speed_callback(const void * msgin);
+  void motor_right_speed_callback(const void * msgin);
+  void motor_direction_callback(const void *msgin);
+
+
+
 void setup() {
+
     pinMode(leftMotor_neg, OUTPUT);
     pinMode(leftMotor_pos, OUTPUT);
     pinMode(rightMotor_neg, OUTPUT);
     pinMode(rightMotor_pos, OUTPUT);  
-    pinMode(imu_sensor, INPUT);   
-    set_microros_transports();  
+
     rcl_allocator_t allocator = rcl_get_default_allocator();  
     rclc_support_t support; 
     rclc_support_init(&support, 0, NULL, &allocator);  
@@ -53,17 +73,12 @@ void setup() {
     rclc_subscription_init_default(&subscriber_right, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, movement_msg_right, Float32), "motor_right_speed");
     rclc_subscription_init_default(&subscriber_direction, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, movement_msg_direction, String), "motor_direction");
 
-    rclc_subscription_set_callback(&subscriber_left, motor_left_speed_callback, NULL);
-    rclc_subscription_set_callback(&subscriber_right, motor_right_speed_callback, NULL);
-    rclc_subscription_set_callback(&subscriber_direction, motor_direction_callback, NULL);
-
-
-    //sexcutor for the subscribers
     rclc_executor_t executor;
-    rclc_executor_init(&executor, &support.context, 1, &allocator);
-    rclc_executor_add_subscription(&executor, &subscriber_left, &movement_msg_left, NULL);
-    rclc_executor_add_subscription(&executor, &subscriber_right, &movement_msg_right, NULL);
-    rclc_executor_spin(&executor);
+    rclc_executor_init(&executor, &support.context, 3, &allocator);
+    rclc_executor_add_subscription(&executor, &subscriber_left, &movement_msg_left, motor_left_speed_callback, ON_NEW_DATA);
+    rclc_executor_add_subscription(&executor, &subscriber_right, &movement_msg_right, motor_right_speed_callback, ON_NEW_DATA);
+    rclc_executor_add_subscription(&executor, &subscriber_direction, &movement_msg_direction, motor_direction_callback, ON_NEW_DATA);
+
 
 }
 
@@ -79,21 +94,12 @@ void motor_right_speed_callback(const void * msgin){
 
 void motor_direction_callback(const void *msgin){
   movement_msg_direction = *(const std_msgs__msg__String *)msgin;
-  direction = movement_msg_direction.data;
+  direction = (string)movement_msg_direction.data;
 }
 
 
 
 void loop() {
-  static unsigned long last_time = 0;
-  unsigned long current_time = millis();
-// publish the data for imu sensor every 1 second
-  if (current_time - last_time >= 1000) {
-    last_time = current_time;
-    int sensor_value = digitalRead(imu_sensor);  
-    msg.data = sensor_value;
-    rcl_publish(&publisher, &msg, NULL); 
-  }
   if(direction == "forward"){
     moveforward(left_speed, right_speed);
   }
